68a94e85c5c0ec94dee66a2c9c459af1f14a2672
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-02-01 13:03:50 +0530

    Create template_string.js

---
+++ template_string.js
@@ -0,0 +1,12 @@
+let name = 'Hitesh'
+   let message = `
+
+      Welcome
+      ${name}
+      to our world
+
+    `;
+
+  // We can embed variables in backticks and other numbers also
+   document.write(message)
+   console.log(message);
